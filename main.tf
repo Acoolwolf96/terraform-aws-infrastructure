0bccbf5909bdc642bde677cb784d3a8a59a1ede8
Acoolwolf96 <njoku.nelson@student.hamk.fi>
2026-04-28 19:52:23 +0300

    add SSH key pair and portfolio deployment

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -13,101 +13,109 @@ provider "aws" {
 
 # VPC 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-    tags = {
-        Name = "${var.app_name}-vpc"
-    }
+  tags = {
+    Name = "${var.app_name}-vpc"
+  }
 }
 
 # Subnet
 resource "aws_subnet" "main" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.1.0/24"
-    availability_zone = "${var.region}a"
-    map_public_ip_on_launch = true
-    tags = {
-        Name = "${var.app_name}-subnet"
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.app_name}-subnet"
+  }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
-    tags = {
-        Name = "${var.app_name}-igw"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "${var.app_name}-igw"
+  }
 }
 
 # Route Table
 resource "aws_route_table" "main" {
-    vpc_id = aws_vpc.main.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
-    tags = {
-        Name = "${var.app_name}-rt"
-    }
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+  tags = {
+    Name = "${var.app_name}-rt"
+  }
 }
 
 # Route Table Association
 resource "aws_route_table_association" "main" {
-    subnet_id      = aws_subnet.main.id
-    route_table_id = aws_route_table.main.id
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
 
 # Security Group
 resource "aws_security_group" "main" {
-    name        = "${var.app_name}-sg"
-    description = "Allow SSH and webtraffic"
-    vpc_id      = aws_vpc.main.id
+  name        = "${var.app_name}-sg"
+  description = "Allow SSH and webtraffic"
+  vpc_id      = aws_vpc.main.id
 
-    ingress {
-        description = "SSH"
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "HTTP"
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        description = "All outbound"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags = {
-        Name = "${var.app_name}-sg"
-    }
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${var.app_name}-sg"
+  }
+}
+resource "aws_key_pair" "main" {
+  key_name   = "homelab-aws"
+  public_key = file("C:\\Users\\nlsnn\\.ssh\\homelab-aws.pub")
 }
 
 # EC2 Instance
 resource "aws_instance" "main" {
-    ami           = "ami-08eb150f611ca277f" # Amazon Linux Ubuntu
-    instance_type  = var.instance_type
-    subnet_id      = aws_subnet.main.id
-    vpc_security_group_ids = [aws_security_group.main.id]
+  ami                    = "ami-08eb150f611ca277f" # Amazon Linux Ubuntu
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name               = aws_key_pair.main.key_name
 
-    user_data = <<-EOF
-                #!/bin/bash
-                apt-get update -y
-                apt-get install -y docker.io
-                systemctl start docker
-                systemctl enable docker
-                docker run -d -p 80:80 nginx:alpine
-                EOF
-    tags = {
-        Name = "${var.app_name}-server"
-    }
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y nginx git
+              systemctl start nginx
+              systemctl enable nginx
+              cd /tmp
+              git clone https://github.com/Acoolwolf96/myapp-gitops
+              cp /tmp/myapp-gitops/index.html /var/www/html/index.html
+              systemctl restart nginx
+              EOF
+  tags = {
+    Name = "${var.app_name}-server"
+  }
 }
